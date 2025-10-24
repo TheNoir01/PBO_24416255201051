deef4204cbac9ce19204cf8ee026cb935c96f58b
TheNoir01 <168410800+TheNoir01@users.noreply.github.com>
2025-10-24 08:56:40 +0700

    tambah code ke branch baru

--- Construct.php
+++ Construct.php
@@ -1,4 +1,25 @@
 <?php
-class Test {
 
-};
+class LuasLingkaran {
+    public const phi = 3.14;
+    public int $jari;
+    public function tampil($nama = 'tabung'):void{
+        $rumus = LuasLingkaran::phi * ($this->jari * $this->jari);
+        echo "Lingkaran {$nama} ini hasilnya adalah: {$rumus}";
+    }
+    public static function testing() {
+        echo "<br/>";
+        echo "Ini testing static";
+    }
+    
+}
+
+$Lingkaran = new LuasLingkaran();
+$Lingkaran->jari = 10; //input nilai jari-jari
+$Lingkaran->tampil("galon"); //tampilkan luas lingkaran
+
+LuasLingkaran::testing(); //memanggil method static
+
+//$rumus = LuasLingkaran::phi * ($Lingkaran->jari * $Lingkaran->jari);
+
+//echo "Hasil nya adalah: " . $rumus;
